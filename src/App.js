7810a3bc86c483ffca343f3375baf9319fb2234f
anthonytruongg <anthonytruongg1@gmail.com>
2022-12-19 20:10:06 -0800

    Initial commit and setup for MemoryGame

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+// import { useState } from 'react';
 
-function App() {
+const  App = () => {
+  const [count, setCount] = useState(0)
+  // call value, then call setValue
+  const [color, setColor] = useState('blue')
+
+  // useEffect (() => {}, []) hook
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === "blue") {
+        setColor("red")
+      }
+      else {
+        setColor("blue")
+      }
+    }
+
+      document.addEventListener("click", changeColorOnClick)
+
+      return () => {
+        document.removeEventListener("click", changeColorOnClick)
+      }
+    }, [color])
+
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      backgroundColor: 'grey',
+    }}>
+       <div className="App" id='myDiv'  style = {{
+      color: "white",
+      width: "100px",
+      height: "100px",
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      backgroundColor: color, // change color that is saved in state
+    }}>
+      Click me :D
+      </div>
+        <div>{count}</div>
+        <button onClick={incrementCount}> Increment </button>
     </div>
+    
   );
 }
 
